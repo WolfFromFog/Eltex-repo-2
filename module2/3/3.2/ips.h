9bf1ac156eafe96635048921b973b02401bad7e8
WolfFromFog <stas.povelitsin@yandex.ru>
2025-11-08 13:05:25 +0000

    Создана структура для хранения адресса, начата работа с main

---
+++ module2/3/3.2/ips.h
@@ -0,0 +1,13 @@
+#ifndef IPS_H
+#define IPS_H
+
+#include <stdint.h>
+
+typedef struct 
+{
+    uint8_t octets[4];
+} ipv4_address ;
+
+
+
+#endif 
--- module2/3/3.2/main.c
+++ module2/3/3.2/main.c
@@ -1,6 +1,30 @@
-#include <ips.h>
+#include "ips.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
+    char*  adress    = (char*)malloc(sizeof(char));
+    char*  mask     = (char*)malloc(sizeof(char));
+    uint16_t N;
+
+    if (argc > 1)
+    {
+
+        adress  = argv[1];
+        mask    = argv[2];
+        N  = (uint16_t)argv[3];
+
+        printf("%s\n%s\n%d", adress, mask, N);
+        
+    }
+    else
+    {
+
+    }
+    
+    free(adress);
+    free(mask);
+
     return 0;
 }
