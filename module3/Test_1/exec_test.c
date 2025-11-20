199914bc90a3dcdc25633dd7dbadf54f4305f56e
WolfFromFog <stas.povelitsin@yandex.ru>
2025-11-20 15:50:02 +0700

    Delete module3/Test_1 directory

--- module3/Test_1/exec_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main(int argc, char *argv[])
-{
-    printf("Run: %s", argv[0]);
-    //execl("hello"," ","Hello", "World!", NULL);
-    execv("hello", argv);
-    return 0;
-}
