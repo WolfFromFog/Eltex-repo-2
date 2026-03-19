deac62f37bbe3070ea6537f921119ab247017d72
WolfFromFog <stas.povelitsin@yandex.ru>
2026-03-19 11:17:23 +0000

    Работа над остальынми скриптами

--- module1/task3.1/cuckoo.sh
+++ module1/task3.1/cuckoo.sh
@@ -6,17 +6,21 @@ FIFO_NAME=cuckoo.pid
 
 mkfifo /tmp/run/$FIFO_NAME 
 
-trap " echo \"$(date '+%d.%m.%Y %H:%M:%S') Shutdown!\">> \"$LOG_FILE\"; exit " SIGINT
+trap ' echo "$(date "+%d.%m.%Y %H:%M:%S") Shutdown!">> "$LOG_FILE"; exit ' SIGINT
+
+exec 3<> "/tmp/run/$FIFO_NAME"
+
 
 echo "$(date '+%d.%m.%Y %H:%M:%S') Startup!">> "$LOG_FILE"
 
 while true; do
 
-	if read line < "/tmp/run/$FIFO_NAME"; then
+	if read -u 3 line; then
 		if [[ "$line" =~ ^([^[]+)\[([0-9]+)\]:\ how\ much\ time\ do\ I\ have\?$ ]]; then
 			client_name="${BASH_REMATCH[1]}"
 			client_pid="${BASH_REMATCH[2]}"
 			NUMBER=$((2 + RANDOM % (10-2+1)))
+			echo "$NUMBER" >&3
 			echo "${client_name}[${client_pid}]: how much time do I have?" >> "$LOG_FILE"
 			echo "$(date '+%d.%m.%Y %H:%M:%S') ${client_name}[${client_pid}] $NUMBER" >> "$LOG_FILE"
 		fi
