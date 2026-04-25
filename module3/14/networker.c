2c02ded04008ea5da46cc6350162d5c2b59c83d2
WolfFromFog <stas.povelitsin@yandex.ru>
2026-04-25 09:44:22 +0000

    Начало работы над 14

---
+++ module3/14/networker.c
@@ -0,0 +1,52 @@
+#include "networker.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int c_wait = 1;
+int sockfd; // сокет
+
+void listener_SIGINT(int sig)
+{
+    _exit(EXIT_SUCCESS);
+}
+
+void sender(int sockfd, struct sockaddr_in *servaddr)
+{
+    char line[1000];
+    while (fgets(line, 1000, stdin) != NULL)
+    {
+        sendto(sockfd, line, strlen(line), 0, (struct sockaddr *)servaddr, sizeof(*servaddr));
+    }
+    close(sockfd);
+    exit(EXIT_SUCCESS);
+}
+
+void recvier(int sockfd)
+{
+    char line[1000];
+    int n;
+    while (1)
+    {
+        n = recvfrom(sockfd, line, 999, 0, NULL, NULL);
+        if (n < 0)
+        {
+            if (errno == EINTR)
+                continue;
+            perror("recvfrom");
+            break;
+        }
+        if (n > 0)
+        {
+            line[n] = '\0';
+            printf("\r%s\n", line);
+            printf("=> ");
+            fflush(stdout);
+        }
+    }
+    close(sockfd);
+    exit(EXIT_SUCCESS);
+}
